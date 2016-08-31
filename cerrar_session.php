c8d8afee444a79229a830b0dd8b2ca34872f1744
sergiopj <sergiopjaranda@gmail.com>
2016-08-31 16:18:58 +0200

    Update cerrar_session.php

--- cerrar_session.php
+++ cerrar_session.php
@@ -1,6 +1,9 @@
 <?php
+//We initiate meeting
 session_start();
+//I erase the record login of the array $session
 unset($_SESSION["login"]);
+//We destroy the meeting and take the user to the index.php
 session_destroy();
 header("Location: index.php");
 exit;
