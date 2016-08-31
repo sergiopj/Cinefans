185be70bf7fe95cdc1375ba58579a74274f08916
sergiopj <sergiopjaranda@gmail.com>
2016-08-31 16:15:49 +0200

    Update alta_obra.php

--- alta_obra.php
+++ alta_obra.php
@@ -1,33 +1,29 @@
 <!doctype html>
-<html lang="">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Alta Obra</title>
 </head>
 <body>
 
-
 <?php
+//If I come here it is that there have been refilled all the fields of the original form, for which all the fields are obligatory with the restricion of html
 
-//si llego aqui es que se han rellenado todos los campos del formulario original, por que todos los campos son obligatorios con la restricion de html
+//To connect as user of the database
+$con=mysql_connect('xxxxwebhost.com','xxxx','xxxx');
 
-
-
-
-//conectarse como usuario de la bd
-$con=mysql_connect('mysql1.000webhost.com','a4376548_sergio','pituspitus');
-
-//codificar texto en utf8 importante si no se verian caracteres raros interpretados por el navegador
+//Text codifies in utf8 importantly if characters not interpreted by the mariner would not appear 
 mysql_query("SET NAMES 'utf8'");
 
-//si hay conexion
-
+//If finally it is possible to connect to the database
 if(isset($con)){
 
-    //como hay conexion insertamos los datos en la bd
-    //selecciono base de datos
-    mysql_select_db("a4376548_cinefan", $con);
-    //preparamos los parametros recogidos por $get y los guardo en variables para trabajar mejor con ellos
+  // Since there is connection we insert the information in the bd 
+  // I select database
+    mysql_select_db("xxxx", $con);
+    
+    //We prepare the parameters gathered for $get and I guard them in variables to work better with them
+    
     $titulo=$_GET['titulo'];
     $year=$_GET['year'];
     $original=$_GET['original'];
@@ -42,23 +38,17 @@ if(isset($con)){
     $sinopsis=$_GET['sinopsis'];
     $reparto=$_GET['reparto'];
     $obra=$_GET['obra'];
-    //consulta donde nos interesa sacar los estrenos
+    
+    //It query where we are interested in extracting the premieres
     $sql = mysql_query("INSERT INTO obras (titulo,año,pais,titulo_ori,sinopsis,musica,fotogra,genero,duracion,trailer,foto,tipo,id_director,reparto) VALUES('$titulo',$year,'$pais','$original','$sinopsis','$musico','$fotografia','$genero',$duracion,'$trailer','$imagen','$obra',(SELECT id_director from directores where nombre_artistico =\"Ann Druyan\"),'$reparto');");
-    //como se ha añadido la nueeva obra en la bd...
+    
+    //Since the new work has been added in the database
     echo "<script languaje='javascript'>
                         alert('Obra $titulo Introducida');
                         location.href = 'Admin.html';
              </script>";
 
 }
-
-
-//si no hay conexion guardamos los errores en el array $error
-
-
-
-
-
 ?>
 
 </body>
