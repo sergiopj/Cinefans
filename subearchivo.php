d682fc5beaf5b7cb1d3a34680f674581cc4d413c
sergiopj <sergiopjaranda@gmail.com>
2016-08-31 18:22:46 +0200

    Update subearchivo.php

--- subearchivo.php
+++ subearchivo.php
@@ -2,27 +2,27 @@
 <html lang="es" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
-    <!--Añado la etiqueta meta-viewport imprecindible para trabajar con bootstrap-->
+    <title>subir archivo</title>
+    <!--I add the label put - viewport imprecindible to work with bootstrap-->
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
     <link rel="stylesheet" href="css/bootstrap.css"/>
     <link rel="stylesheet" href="css/estilos.css"/>
     <style>
 
         div{
-
             position: absolute;
             width: 60%;
             height: 20%;
             top:30%;
             left: 40%;
-
         }
 
     </style>
 </head>
 <body>
 <?php
+
+//I check Received file
 
 
     if ($_FILES['archivo']["error"] > 0)
@@ -33,13 +33,7 @@
     else
     {
 
-
-
-
-
-
-
-        /*ahora co la funcion move_uploaded_file lo guardaremos en el destino que queramos*/
+        //move the received file to server
 
         move_uploaded_file($_FILES['archivo']['tmp_name'],
 
@@ -49,10 +43,6 @@
                         alert('Archivo Subido');
                         location.href = 'test_subida.php';
                         </script>";
-
-
-
-
 }
  ?>
 
