1b1ce1b16e098310cb563fd63fccf74654115f39
sergiopj <sergiopjaranda@gmail.com>
2016-08-31 18:50:01 +0200

    Update votos.php

--- votos.php
+++ votos.php
@@ -1,74 +1,59 @@
 
 <?php
-//iniciamos session pues nos hace falta nombre de usuario guardado en $_SESSION
-
 session_start();
-
-
 ?>
 
-
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Votos</title>
 </head>
-<!--Añado la libreria jquery-->
+
+<!--I Add the jquery library-->
 <script src="js/jquery.js.js"></script>
-<!--Acceso al archivo bootstrap con el js-->
+<!--I Access to the file bootstrap with the js-->
 <script src="js/bootstrap.min.js"></script>
+
 <body>
 
 <?php
     if(isset($_SESSION['usuario']) and isset($_GET['valor_voto'])){
 
-        //lo recogo de $get y de $session
+        //i received from $get and $session
          $valor=$_GET['valor_voto'];
          $user=$_SESSION['id_user'];
          $id_obra=$_GET['idpeli'];
 
 
-        //conectarse como usuario de la bd
-        $con=mysql_connect('mysql1.000webhost.com','a4376548_sergio','pituspitus');
+        
+        //To connect as user of the database
+        $con=mysql_connect('xxxxwebhost.com','xxxx','xxxx');
 
-        //codificar texto en utf8 importante si no se verian caracteres raros interpretados por el navegador
+        //Text codifies in utf8 importantly if characters not interpreted by the web navigator
         mysql_query("SET NAMES 'utf8'");
 
-        //si hay conexion preparamos la consulta
+        //If we connect we do the query and select the database
 
         if($con){
 
-            //selecciono base de datos
-            mysql_select_db("a4376548_cinefan", $con);
+            //select the database
+            mysql_select_db("xxxx", $con);
 
             $sql = mysql_query("insert into puntuaciones(valor,fecha,id_obra,id_user) values($valor,null,$id_obra,$user)");
 
-            //aviso de voto y vuelta al index sin perder la session
+            //I warn of vote applied and turned to the index.php without losing the session
 
             echo"<script languaje='javascript'>
                     alert('Has votado');
                     location.href = 'index.php';
                     </script>";
 
-
-
         }
-
-
-
-
-
-        //nos disponemos a votar , tenemos id_user,id_obra y valor del voto
-
 
     }
 
-
-
 ?>
-
-
 
 </body>
 </html>
