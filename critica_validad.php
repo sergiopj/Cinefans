a4eb9b738296b8cbd3a4a74d8727af043297fad1
sergiopj <sergiopjaranda@gmail.com>
2016-08-31 16:28:45 +0200

    Update critica_validad.php

--- critica_validad.php
+++ critica_validad.php
@@ -1,79 +1,60 @@
 <?php
 session_start()
 ?>
-
-
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Validacion de Critica</title>
 </head>
 <body>
-
-
 <?php
-//me llega el texto de la critica por $get y la id de a peli por $session
-//me aseguro que hay session y de que llega la critica sin estar vacia
+// The text of the critique comes to me for $get and her go of to peli for $session
+// I assure myself that it is session and from that the critique comes without being empty
 
 if(isset($_SESSION['usuario'])&& isset($_SESSION['id_obra']) && isset($_GET['autor']) && isset($_GET['contenido']) && !$_GET['autor']=="" && !$_GET['contenido']==""){
 
-   //conectamos a la bd e insertamos la critica
+   //We connect to the database and insert the critique
 
-    //conectarse como usuario de la bd
-    $con=mysql_connect('mysql1.000webhost.com','a4376548_sergio','pituspitus');
+    //To connect as user of the database
+    $con=mysql_connect('xxxxwebhost.com','xxxx','xxxx');
 
-    //selecciono base de datos
-    mysql_select_db("cinefan", $con);
+    //i select the database
+    mysql_select_db("xxxx", $con);
 
-    //codificar texto en utf8 importante si no se verian caracteres raros interpretados por el navegador
+    //Text codifies in utf8 importantly if characters not interpreted by the mariner would not appear 
     mysql_query("SET NAMES 'utf8'");
 
-    //preparo las variables
+    //i make the variables
      $usuario=$_SESSION['id_user'];
      $obra=$_SESSION['id_obra'];
      $critica=$_GET['contenido'];
      $autor=$_GET['autor'];
 
-    //si hay conexion
+    //If there is connection
     if($con){
 
-        //selecciono base de datos
-        mysql_select_db("a4376548_cinefans", $con);
-        //consulta donde nos interesa sacar los estrenos
+        //select the database
+        mysql_select_db("xxxx", $con);
+        //It query where we are interested in extracting the premieres
         $sql = mysql_query("insert into criticas(texto,autor,fecha,id_obra,id_user) values('$critica','$autor',null,$obra,$usuario)");
-
-
-
 
 
         echo"<script languaje='javascript'>
                     alert('Ha Escrito su crítica');
                     location.href = 'index.php';
                     </script>";
-
-
-
     }
 
-    //si no hay conexion
-    else{
-
-
-
-    }
+    
 }
 
-
 else{
-
-    //si el texto viene vacio...
-
+    //If the text comes emptiness...
     echo"<script languaje='javascript'>
                     alert('¡Hay campos vacios!');
                     location.href = 'index.php';
                     </script>";
-
 }
 
 
